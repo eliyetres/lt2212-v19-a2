fe1f4981fc284cc2d6e18d4a2535b0cfe202a6f7
eliyetres <eliyetres@hotmail.com>
2019-02-12 10:38:17 +0000

    test

--- gendoc.py
+++ gendoc.py
@@ -1,4 +1,5 @@
-import os, sys
+import os
+import sys
 import glob
 import argparse
 import numpy as np
@@ -10,9 +11,11 @@ from sklearn.feature_extraction.text import TfidfTransformer
 # in so that we better understand what you're doing when we grade!
 
 # add whatever additional imports you may need here
+import re
 
 parser = argparse.ArgumentParser(description="Generate term-document matrix.")
-parser.add_argument("-T", "--tfidf", action="store_true", help="Apply tf-idf to the matrix.")
+parser.add_argument("-T", "--tfidf", action="store_true",
+                    help="Apply tf-idf to the matrix.")
 parser.add_argument("-S", "--svd", metavar="N", dest="svddims", type=int,
                     default=None,
                     help="Use TruncatedSVD to truncate to N dimensions")
@@ -37,9 +40,51 @@ if args.tfidf:
     print("Applying tf-idf to raw counts.")
 
 if args.svddims:
-    print("Truncating matrix to {} dimensions via singular value decomposition.".format(args.svddims))
+    print("Truncating matrix to {} dimensions via singular value decomposition.".format(
+        args.svddims))
 
 # THERE ARE SOME ERROR CONDITIONS YOU MAY HAVE TO HANDLE WITH CONTRADICTORY
 # PARAMETERS.
 
 print("Writing matrix to {}.".format(args.outputfile))
+
+
+# FUNCTIONS
+
+def readfile(filename):
+    """ Opens and reads a file """
+    f = open(filename, "r")
+    text = f.read()
+    return text
+
+
+def preprocess(text):
+    """ Lowercases the text of a file and removes punctuation marks. """
+    pp_text = []
+    for line in text:
+        line = line.lower()
+        p = re.sub(r'[^\w\s]', '', line)
+        pp_text.append(p)
+    return pp_text
+
+
+def topm(m):
+    """ If -B is set,  only the top-m most frequent word by count will be used, the rest will be filtered out. If it isn't set, all words will be used. (This is a failsafe in case the  documents doesnt fit in memory.) """
+    freqwords = 0
+    return freqwords
+
+
+def loaded_counts(filemame):
+    """ If -T is set, the loaded counts (after filtering by the -B option) will be transformed from raw counts into tf-idf values. """
+    # count
+    # tf-idf values
+    tfvalues = 0
+    return tfvalues
+
+
+def docmatrix(doc):
+    """ If -S is set, then the term-document matrix will be transformed by klearn's TruncatedSVD  operation into a document matrix with a feature space of dimensionality n. """
+    # document
+    # matrix
+    d_matrix = 0
+    return d_matrix
--- simdoc.py
+++ simdoc.py
@@ -1,4 +1,5 @@
-import os, sys
+import os
+import sys
 import argparse
 import numpy as np
 import pandas as pd
@@ -8,11 +9,26 @@ from sklearn.metrics.pairwise import cosine_similarity
 # in so that we better understand what you're doing when we grade!
 
 # add whatever additional imports you may need here
+import re
 
-parser = argparse.ArgumentParser(description="Compute some similarity statistics.")
+parser = argparse.ArgumentParser(
+    description="Compute some similarity statistics.")
 parser.add_argument("vectorfile", type=str,
-                    help="The name of the input  file for the matrix data.")
+                    help="The name of the input file for the matrix data.")
 
 args = parser.parse_args()
 
 print("Reading matrix from {}.".format(args.vectorfile))
+
+
+
+def average_sim(text):
+    """" Prints four values, two for each topic.  
+    
+    For each of the two topics, print the average similarity of every document vector with that topic to every other vector with that same topic (for simplicity, including itself, if you want), averaged over the entire topic. the average similarity of every document vector with that topic to every document vector in the other topic, averaged over the entire topic. """
+
+
+def print_table(results):
+    """ Prints the results to the console """
+    res = 0
+    return res
